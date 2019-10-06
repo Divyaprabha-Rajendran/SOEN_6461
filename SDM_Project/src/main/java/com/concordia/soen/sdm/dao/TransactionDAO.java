ed861ac9b1a6cca2a2bee47e31566fe94881d3ec
bhavana <bomma08@gmail.com>
2019-10-06 15:56:40 -0400

    updated javadoc for TRANSACTIONDAO

--- SDM_Project/src/main/java/com/concordia/soen/sdm/dao/TransactionDAO.java
+++ SDM_Project/src/main/java/com/concordia/soen/sdm/dao/TransactionDAO.java
@@ -16,6 +16,12 @@ public class TransactionDAO {
 	  DataSource datasource;
 	  @Autowired
 	  JdbcTemplate jdbcTemplate;
+	  
+	  /**
+	   * getTransactiopnID is a method that takes reservation id as an argument and searches with reservationid in the database
+	   * @param reservationId is of type int 
+	   * @return
+	   */
 	  public Transaction getTransactionID(int reservationId) {
 		  System.out.println("DAO:Start");
 		  String sql = "SELECT * FROM rentedVehiclesRecord  WHERE reservationId=?";
@@ -25,6 +31,12 @@ public class TransactionDAO {
 	                BeanPropertyRowMapper.newInstance(Transaction.class));
 		  
 	  }
+	  
+	  /**
+	   * it gets the transaction status by taking the status of the 
+	   * @param status is of type string 
+	   * @return
+	   */
 	  public Transaction getTransactionStatus(String status) {
 		  System.out.println("DAO:Start");
 		  String sql = "SELECT status FROM rentedVehiclesRecord where reservationId=?;";
@@ -33,7 +45,11 @@ public class TransactionDAO {
                 BeanPropertyRowMapper.newInstance(Transaction.class));
 		  
 	  }
-	  
+	  /**
+	   * get return save method takes status as argument and update the rented vehicle record and then sets it available
+	   * @param status
+	   * @return
+	   */
 	  public Transaction getreturnsave(Transaction status) {
 		  System.out.println("DAO:Start");
 		  String sql = "update rentedVehiclesRecord set status=\"available\";";
@@ -42,6 +58,12 @@ public class TransactionDAO {
 	                BeanPropertyRowMapper.newInstance(Transaction.class));
 		  
 	  }
+	  
+	  /**
+	   * this method insert data is to enter all the details for renting the vehicle 
+	   * @param transaction
+	   */
+	  
 	public void insertData(Transaction transaction) {
 		String insertQuery = "INSERT INTO VehicleRentingSystem.rentedVehiclesRecord(startdate,duedate,licenseNumber,licensePlate, status, cost) VALUES ('"
 				+ transaction.getStartdate()+"','"
@@ -52,6 +74,12 @@ public class TransactionDAO {
 				+ transaction.getCost()+"')";
 		jdbcTemplate.execute(insertQuery);
 	}
+	
+	
+	/**
+	 * getAllTransactions method gets all details of all  the transactions of the vehicles from the system 
+	 * @return
+	 */
 	public List<Transaction> getAllTransactions() {
 		String sql = "SELECT * FROM VehicleRentingSystem.rentedVehiclesRecord";
 		return jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Transaction.class));
