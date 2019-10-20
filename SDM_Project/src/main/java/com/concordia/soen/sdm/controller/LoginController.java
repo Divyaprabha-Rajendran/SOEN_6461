34b8c6893696ee5155ef7d6b0f6485f177e8980b
Mahy <mahyhanysalama@gmail.com>
2019-10-20 19:27:39 -0400

    Adding login functionality for Administrator

--- SDM_Project/src/main/java/com/concordia/soen/sdm/controller/LoginController.java
+++ SDM_Project/src/main/java/com/concordia/soen/sdm/controller/LoginController.java
@@ -17,41 +17,54 @@ import com.concordia.soen.sdm.pojo.UserLogin;
 @RequestMapping("/clerk/*")
 public class LoginController {
 
-	@Autowired
-	UserLoginDAO  userLoginDAO;
-	@Autowired 
-	private HttpSession httpSession;
-	/**
-	 * This method is for the clerk login. The clerk gets logged in by entering username and password 
-	 * @param request
-	 * @param response of type httpservlet response
-	 * @return
-	 */
-	@RequestMapping(value ="/clerk/login", method = {RequestMethod.POST, RequestMethod.GET})
-	public ModelAndView login(HttpServletRequest request,
-			HttpServletResponse response) {
-//		System.out.println(httpSession.getAttribute("clerkName"));
-		String userName;
-		ModelAndView mv=new ModelAndView();
-		if(httpSession.getAttribute("clerkName") != null) {
-			mv.setViewName("welcome");
-			userName = (String) httpSession.getAttribute("clerkName");
-		}else {
-			mv.setViewName("welcome");
-			userName=request.getParameter("username");  
-			String password=request.getParameter("password");
-			try {
-				UserLogin user =  userLoginDAO.userLogin(userName,password);				
-				httpSession.setAttribute("clerkName", user.getUserName());
-				String msg;
-				msg = "Welcome " +userName + ".";
-				mv.addObject("message",msg);
-				System.out.println("LoginController:Stop");
-			}catch (EmptyResultDataAccessException e) {
-				mv.setViewName("redirect:/");
-			}
-		}
-		return mv;
-	}
+@Autowired
+UserLoginDAO  userLoginDAO;
+@Autowired
+private HttpSession httpSession;
+/**
+* This method is for the clerk login. The clerk gets logged in by entering username and password
+* @param request
+* @param response of type httpservlet response
+* @return
+*/
+@RequestMapping(value ="/clerk/login", method = {RequestMethod.POST, RequestMethod.GET})
+public ModelAndView login(HttpServletRequest request,
+HttpServletResponse response) {
+// System.out.println(httpSession.getAttribute("clerkName"));
+String userName;
+ModelAndView mv=new ModelAndView();
+
+
+userName=request.getParameter("username");  
+String password=request.getParameter("password");
+try {
+UserLogin user =  userLoginDAO.userLogin(userName,password);
+if(user.getRole().equals("clerk"))
+{
+httpSession.setAttribute("clerkName", user.getUserName());
+String msg;
+mv.setViewName("welcome");
+msg = "Welcome " +userName + ".";
+//System.out.println(msg);
+mv.addObject("message",msg);
+}
+else if(user.getRole().contentEquals("admin"))
+{
+httpSession.setAttribute("adminName", user.getUserName());
+String msg;
+mv.setViewName("welcomeAdmin");
+msg = "Welcome " +userName + ".";
+//System.out.println(msg);
+mv.addObject("message",msg);
+}
+System.out.println("LoginController:Stop");
+}
+catch (EmptyResultDataAccessException e)
+{
+System.out.println(e);
+mv.setViewName("redirect:/");
 }
 
+return mv;
+}
+}
---
+++ SDM_Project/src/main/webapp/WEB-INF/views/welcomeAdmin.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+
+    pageEncoding="ISO-8859-1"%>
+
+    <%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  </head>
+
+  <body>
+
+    <h2>${message}</h2>
+
+    <h3><a href="<c:url value="/system/catalog"/>">View Catalog</a></h3>
+
+    <h3><a href="<c:url value="/client/dashboard"/>">Vehicle Management System</a></h3>
+
+    <h3><a href="<c:url value="/system/catalog?from=reserve"/>">View Transactions </a></h3>
+
+    <h3><a href="<c:url value=""/>">Vehicle Record Management </a></h3>
+
+    </body>
+
+      <a href="${contextpath }/root/">Logout</a>
+
+</html>
+
