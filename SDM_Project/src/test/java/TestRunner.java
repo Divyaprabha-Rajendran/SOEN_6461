f5cdc1bfd6b7e30a507b2a6389f730ac69263983
Harish Jayasankar <harishjayasankar28@gmail.com>
2019-10-25 16:08:56 -0400

    Committing junit for transaction history functionality-transaction controller

--- SDM_Project/src/test/java/TestRunner.java
+++ SDM_Project/src/test/java/TestRunner.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({TestCatalogController.class,
-TestClient.class
+TestClient.class,TestTransactionController.class
 })
  
 public class TestRunner {
