0b3da48f5010dc2fe21bf9aeffe2da23ba1e931f
Divyaprabha Rajendran <d_ajendr@encs.concordia.ca>
2019-11-17 23:16:07 -0500

    modified VehicleReservationController

--- SDM_Project/src/main/java/com/concordia/soen/sdm/controller/VehicleReservationController.java
+++ SDM_Project/src/main/java/com/concordia/soen/sdm/controller/VehicleReservationController.java
@@ -1,5 +1,6 @@
 package com.concordia.soen.sdm.controller;
 
+import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,12 +21,14 @@ import org.springframework.web.servlet.ModelAndView;
 import com.concordia.soen.sdm.dao.CatalogDAO;
 import com.concordia.soen.sdm.dao.ClientDAO;
 import com.concordia.soen.sdm.dao.TransactionDAO;
+import com.concordia.soen.sdm.mapper.ClientManagementMapper;
+import com.concordia.soen.sdm.mapper.VehicleReservationMapper;
 import com.concordia.soen.sdm.pojo.CatalogDetails;
 import com.concordia.soen.sdm.pojo.Client;
 import com.concordia.soen.sdm.pojo.Transaction;
 
 /**
- * @author Divyaprabha Rajendran
+ *
  * Controller for making reservation or rental.
  *
  */
@@ -33,14 +37,17 @@ import com.concordia.soen.sdm.pojo.Transaction;
 public class VehicleReservationController {
 
 	@Autowired
-	CatalogDAO catalogDao;
-
-	@Autowired
-	ClientDAO clientDao;
+	private HttpSession httpSession;
+	
 	
 	@Autowired
 	TransactionDAO transactionDao;
 	
+	@Autowired
+	VehicleReservationMapper vehicleReservationMapper;
+	
+	@Autowired
+	ClientManagementMapper clientManagementMapper;
 	/**
 	 * Method to process the reservation or rental request
 	 * @param HTTPRequest containing the license plate  and license number  
@@ -51,53 +58,78 @@ public class VehicleReservationController {
 	@RequestMapping(value="/reserve")
 	public ModelAndView reserve(HttpServletRequest request) throws Exception {
 		ModelAndView view = new ModelAndView("vehicle_reservation");
-
 		if(request.getParameter("startDate") != null) {
-			SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); // your template here
+			SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm"); // your template here
 			
 			Transaction transaction = new Transaction();
-			String licensePlate = request.getParameter("licenseNumber");
-			if(isValidLicenseuser(licensePlate)) {
-				transaction.setLicensePlate(request.getParameter("licensePlate"));
-				transaction.setLicenseNumber(request.getParameter("licenseNumber"));
+			String licenseNumber = request.getParameter("licenseNumber");
+			Client client=isValidLicenseuser(licenseNumber);
+			if(client!=null) {
+				String licensePlate=request.getParameter("licensePlate");
+				///String licenseNumber=request.getParameter("licenseNumber"));
+				int vehicleId=(int) httpSession.getAttribute("reserveVehicleId");
 				String date = request.getParameter("endDate");
 				date = date.replace("T", " ");
 				String dateTime = formater.format(formater.parse(date));
-				transaction.setDuedate(dateTime);
+				String duedate=(dateTime);
 				date = request.getParameter("startDate");
 				date = date.replace("T", " ");
 				dateTime = formater.format(formater.parse(date));
-				transaction.setStartdate(dateTime);
-				transaction.setStatus(findStatus(date));
-				transaction.setCost(Integer.parseInt(request.getParameter("cost")));
-				catalogDao.updateAvailability("NO", request.getParameter("licensePlate"));
-				transactionDao.insertData(transaction);
-				view.addObject("message", "Successfully Stored");
+				String startdate=(dateTime);
+				String status=(findStatus(date));
+				int Cost=Integer.parseInt(request.getParameter("cost"));
+				///Client clientDetails =vehicleReservationMapper.select(licenseNumber);
+				int clientId=(client.getClientId());
+				//System.out.println("VehicleiD:"+httpSession.getAttribute("reserveVehicleId"));
+			//	System.out.println("ClientID:Reservation:"+clientDetails.getClientId());
+				//transaction.setClientId(clientDetails.getClientId());
+				
+	
+				if(status.equalsIgnoreCase("rented")) {
+				vehicleReservationMapper.updateAvailability("NO", request.getParameter("licensePlate"));}
+			boolean reservationStatus=vehicleReservationMapper.insertData(licenseNumber,licensePlate,duedate,startdate,status,Cost,clientId,vehicleId);
+			//	transactionDao.insertData(licenseNumber,licensePlate,duedate,startdate,status,Cost,clientId,vehicleId);
+				if(reservationStatus==true) {
+				view.addObject("message", "Reservation Successful");}
+				else {
+					view.addObject("message", "Reservation is unsuccessful");
+				}
+				httpSession.setAttribute("reserveVehicleId",null);
+				
 			}else {
 				view.addObject("message", "Invalid LicensePlate");
-			}
+				httpSession.setAttribute("reserveVehicleId",null);
+			} 
 
 		}else {
-			CatalogDetails vehicleDetails = catalogDao.getVehicleDetails(request.getParameter("licensePlate"));
+			CatalogDetails vehicleDetails = vehicleReservationMapper.getVehicle(request.getParameter("licensePlate"));
+				
+			httpSession.setAttribute("reserveVehicleId", vehicleDetails.getVehicleId());
+			//	catalogDao.getVehicleDetails(request.getParameter("licensePlate"));
 			view.addObject("vehicleDetails", vehicleDetails);
 			Map<String, String> userData = new HashMap<String, String>();
-			List<Client> clientDetails = clientDao.getAllClientDetails();
+			List<Client> clientDetails = vehicleReservationMapper.getAllClientDetails();
+				//	clientDao.getAllClientDetails();
 			clientDetails.stream().forEach(data -> {
-				String values = data.getFirstName()+","+data.getLastName()+","+data.getPhoneNo();
+				String values = data.getFirstName()+","+data.getLastName()+","+data.getPhoneNo()+","+data.getClientId();
 				userData.put(data.getLicenseNumber(), values);
 			});
 			view.addObject("user", userData);
+			
 		}
+		
 		return view;
 	}
 
-	private boolean isValidLicenseuser(String licensePlate) {
+	private Client isValidLicenseuser(String licenseNumber) {
+		Client client =null;
 		try {
-			Client client = clientDao.getClientDetails(licensePlate);
-			return true;
+			 client =vehicleReservationMapper.select(licenseNumber);;
+			
 		} catch (Exception e) {
-			return false;
+		
 		}
+		return client;
 	}
 
 	@ResponseBody
@@ -109,7 +141,17 @@ public class VehicleReservationController {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		Date sDate = format.parse(startDate);
 		Date eDate = format.parse(endDate);
-		List<Transaction> records = transactionDao.getVehicleRentRecords(numberPlate);
+		List<Transaction> records=null;
+		try {
+			records = vehicleReservationMapper.getVehicleRentRecords(numberPlate);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				//transactionDao.getVehicleRentRecords(numberPlate);
 		for(int i = 0; i < records.size(); i++) {
 			Transaction data = records.get(i);
 			Date dbStartDate = format.parse(data.getStartdate());
@@ -138,7 +180,7 @@ public class VehicleReservationController {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
 		Date std = format.parse(startDate);
 		if (std.compareTo(date) == 0) {
-			return "rental";
+			return "rented";
 		}else {
 			return "reserved";
 		}
