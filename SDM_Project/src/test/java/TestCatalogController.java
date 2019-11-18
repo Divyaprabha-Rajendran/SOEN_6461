6262f0365c3f198cd1e1af00ecee7e2a6fa17d43
bhavana <bomma08@gmail.com>
2019-11-18 00:44:22 -0500

    Junit for TestCatalogController

--- SDM_Project/src/test/java/TestCatalogController.java
+++ SDM_Project/src/test/java/TestCatalogController.java
@@ -1,12 +1,26 @@
 import static org.junit.Assert.assertNotNull;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.junit.Test;
 
-import com.concordia.soen.sdm.dao.CatalogDAO;
+
+import com.concordia.soen.sdm.tableDataGateway.ClerkCatalogTableDataGateway;
 
 public class TestCatalogController {
-
-CatalogDAO cl= new CatalogDAO();
+ResultSet rs;
+ClerkCatalogTableDataGateway cl= new ClerkCatalogTableDataGateway();
 @Test public void test() {
-assertNotNull(cl);}
+try {
+rs = cl.selectMultipleRows();
+} catch (ClassNotFoundException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+} catch (SQLException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
 }
+assertNotNull(rs);}
+}
+
